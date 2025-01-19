9f238b8c72b49ce5b57f8529bad51fb3a1d6fbb5
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-01-19 07:42:36 -0500

    Update StoryMenuState.hx

--- source/states/StoryMenuState.hx
+++ source/states/StoryMenuState.hx
@@ -13,6 +13,7 @@ import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
 import lime.net.curl.CURLCode;
+import Controls;
 
 #if windows
 import Discord.DiscordClient;
